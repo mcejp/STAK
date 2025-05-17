5f8cbf135028466999d396caf69533890cef844f
Martin Cejp <cejp.mart@gmail.com>
2025-05-17 17:28:21 +0200

    04input: Erase just the square instead of the entire screen

--- 04input.scm
+++ 04input.scm
@@ -1,12 +1,20 @@
 (define (main)
-  (define x 100)
-  (define y 100)
+  (define bk-color COLOR:WHITE)
+  (define w 30)
+  (define h 25)
+  (define x (/ (- W w) 2))
+  (define y (/ (- H h) 2))
   (define speed 4)
 
+  (fill-rect bk-color 0 0 W H)
+
   (while 1
-    (fill-rect 15 0 0 W H)
-    (fill-rect 0 x y 30 30)
+    ;; draw
+    (fill-rect COLOR:LIGHTRED x y w h)
+    ;; display
     (pause-frames 1)
+    ;; erase
+    (fill-rect bk-color x y w h)
 
     (when (key-held? KEY:UP)    (set! y (- y speed)))
     (when (key-held? KEY:DOWN)  (set! y (+ y speed)))
